d4fe134e76d42672ee2f3862618ddd8dd4b8f6cc
Sergey Dayneko, Ph.D <s.daineko@gmail.com>
2025-01-19 21:28:58 -0800

    update 250119

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import { MainNav } from "@/components/main-nav";
 
 export default function Home() {
   return (
     <>
+      <MainNav />
        <main className="flex flex-col items-start justify-evenly mt-16 md:flex-row">
-        PV Sensors 
        </main>
     </>
   );
