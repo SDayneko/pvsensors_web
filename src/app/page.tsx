8de007fb533f53bf909f2c0c83b3123eb6170a8a
Sergey Dayneko, Ph.D <s.daineko@gmail.com>
2025-01-19 21:52:55 -0800

    update 250119

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
     <>
        <main className="flex flex-col items-start justify-evenly mt-16 md:flex-row">
         <h1>PV Sensors</h1>
+        <h2>blog</h2>
        </main>
     </>
   );
