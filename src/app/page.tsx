1527cd1ac85478e8224f138e6d55652e0792bbfd
Sergey Dayneko, Ph.D <s.daineko@gmail.com>
2025-01-19 21:03:14 -0800

    update 250119

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
+       <main className="flex flex-col items-start justify-evenly mt-16 md:flex-row">
+        PV Sensors 
+       </main>
     </>
   );
 }
